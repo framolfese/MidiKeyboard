64cb7c9b3e0d2f4452a9635462abcfab4d3741dd
framolfese <molfesefrancesco@gmail.com>
2019-03-10 16:16:52 +0100

    git pull

---
+++ Codice PC/serialreader.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main() {
+    FILE* seriale = fopen("/dev/ttyACM0", "r"); 
+    if (seriale == NULL) { 
+        printf("Couldn't open file"); 
+        exit(0); 
+    }
+    while(1){
+        int parola = fgetc(seriale);
+        printf("%d\n", parola);
+    }
+    fclose(seriale);
+    return 0;
+}
