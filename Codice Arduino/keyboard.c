e742777c07afddcdbf27a07afc7c0e1a3c1da2c9
fuocoman96 <douglasscottidivigoleno.1743635@studenti.uniroma1.it>
2019-03-10 19:05:55 +0100

    now the arduy cna send to the serial the struct

--- Codice Arduino/keyboard.c
+++ Codice Arduino/keyboard.c
@@ -5,40 +5,41 @@
 #include "../avr_common/uart.h" // this includes the printf and initializes it
 #include <keyboard.h>
 
-
-
-int main(void){
+int main(void)
+{
   // this initializes the printf/uart thingies
-  printf_init(); 
- 
-  uint8_t mask;  //(1<<2);
+  printf_init();
+
+  uint8_t mask; //(1<<2);
   int i;
-  for(i=2;i<6;i++){
-  mask = (1<<i);
-  DDRF &= ~mask;
-  PORTF |= mask;
+  for (i = 2; i < 6; i++)
+  {
+    mask = (1 << i);
+    DDRF &= ~mask;
+    PORTF |= mask;
   }
 
   //initialize the ToneStruc Vector
   Tone toneStructVector[8];
-  toneStructVector[2].value='F';
-  toneStructVector[3].value='M';
-  toneStructVector[4].value='R';
-  toneStructVector[5].value='D';
+  toneStructVector[2].value = 'F';
+  toneStructVector[3].value = 'M';
+  toneStructVector[4].value = 'R';
+  toneStructVector[5].value = 'D';
 
+  while (1)
+  {
+    for (i = 2; i < 6; i++)
+    {
+      mask = (1 << i);
+      int key = (PINF & mask) == 0;
+      if (key != 0)
+      {
 
-  while(1){
-    for(i=2;i<6;i++){
-    mask = (1<<i);
-    int key=(PINF&mask)==0; 
-    if(key!=0){
-      
-      
-      printf("switch %02x, %d, sono la nota %c,\n", (int) PORTF, key,toneStructVector[i].value);
-      printf("la struct %p\n",toneStructVector[i]);
+        //printf("switch %02x, %d, sono la nota %c,\n", (int) PORTF, key,toneStructVector[i].value);
+        //printf("la struct %p\n",toneStructVector[i]);
+        SendToSerial(toneStructVector[i]);
       }
     }
     _delay_ms(100); // from delay.h, wait 1 sec
   }
-  
 }
--- Codice Arduino/keyboard.h
+++ Codice Arduino/keyboard.h
@@ -1,5 +1,27 @@
+#include <string.h>
 
-
-typedef struct Tone{
+typedef struct Tone
+{
     char value;
+    int checksum;
 } Tone;
+
+int SendToSerial(Tone tone)
+{
+
+    char buffer[sizeof(Tone)];
+    memcpy(buffer, &tone, sizeof(Tone));
+    int i = 0;
+    for (i; i < sizeof(Tone); i++)
+        printf(buffer + i);
+    printf("\n");
+    /*
+  char *p = (char*) &tone;
+  int i;
+  for(i=0; i<sizeof(tone); i++) 
+   {
+   putcUSART(*p++);
+   }
+   */
+    return 0;
+}
