84e84f52089f8b23277e476e493c6115689d6f82
francesco douglas scotti di vigoleno <douglasscottidivigoleno.1743635@studenti.uniroma1.it>
2019-03-11 15:39:41 +0000

    fixing lettura del tasto

--- Codice Arduino/keyboard.c
+++ Codice Arduino/keyboard.c
@@ -36,44 +36,58 @@ int main(void)
   // this initializes the printf/uart thingies
   printf_init();
 
-  uint8_t mask; //(1<<2);
-  int i;
-  for (i = 2; i < 6; i++)
-  {
-    mask = (1 << i);
+    uint8_t mask; //(1<<2);
+    int i;
+    mask = 0x1E;
     DDRF &= ~mask;
     PORTF |= mask;
-  }
-
+  
   //initialize the ToneStruc Vector
   Tone toneStructVector[8];
   toneStructVector[2].value = 'F';
-  toneStructVector[2].some_value = 1;
+  toneStructVector[2].some_value = 0;
  
   toneStructVector[3].value = 'M';
-  toneStructVector[3].some_value = 2;
+  toneStructVector[3].some_value = 0;
   
   toneStructVector[4].value = 'R';
-  toneStructVector[4].some_value = 3;
+  toneStructVector[4].some_value = 0;
   
   toneStructVector[5].value = 'D';
-  toneStructVector[5].some_value = 4;
+  toneStructVector[5].some_value = 0;
 
+    
+  uint8_t lettura, lettura_prec;
+  
   while (1)
   {
-    for (i = 2; i < 6; i++)
+    lettura = PINF & mask;
+    for (int i=2;i<6;++i){
+        if(((lettura>>i)&0x1) != ((lettura_prec>>i)&0x1)) {
+            // cambiamento
+            // trasmetti (lettura>>i)&0x01 che vale 0 o 1 alla pressione 
+            SendToSerial(&toneStructVector[i]);
+            toneStructVector[5].some_value = (lettura>>i)&0x01;
+        }
+    }
+    
+    lettura_prec=lettura
+    
+    /*for (i = 2; i < 6; i++)
     {
       mask = (1 << i);
       int key = (PINF & mask) == 0;
-      if (key != 0)
+      if (mask!=lastMask )
       {
-
+        lastMask=mask;
         //printf("switch %02x, %d, sono la nota %c,\n", (int) PORTF, key,toneStructVector[i].value);
         //printf("la struct %p\n",toneStructVector[i]);
     
         SendToSerial(&toneStructVector[i]);
       }
     }
-    _delay_ms(100); // from delay.h, wait 1 sec
+    */
+    
+    //_delay_ms(50); // from delay.h, wait 1 sec
   }
 }
