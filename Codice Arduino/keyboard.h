acdd59c349bd1ffd1618cfd2f037a191d450e057
francesco douglas scotti di vigoleno <douglasscottidivigoleno.1743635@studenti.uniroma1.it>
2019-03-11 15:11:39 +0000

    Update sneder

--- Codice Arduino/keyboard.h
+++ Codice Arduino/keyboard.h
@@ -1,10 +1,21 @@
+
 #include <string.h>
 
+#pragma pack(push,1)
 typedef struct Tone
 {
-    int marco;
+    uint8_t some_value;
     char value;
     
 } Tone;
+#pragma pack(pop)
+
+//this funcition provides synchronization over the uart
+// 0XAA
+// 0X55
+// 0XAA
+// Struct Tone di dimensione sizeof(Tone)
+// eventuale checksum che implementeremo per adesso invio "0"
+// 0X55 questo serve per dire di aver finito i pacchetti inviati
 
 int SendToSerial(Tone *tone);
